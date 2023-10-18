b0e9dfa8330c2ac64ac0d30fdfa47852cbfa6651
Chiran Gurung <grgchiran08@yahoo.com>
2023-10-17 21:39:56 -0400

    fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./components/Main";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <>
+    <Router>
       <Nav />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        {/* <Route path="/product-details" element={<ProductDetails />} /> */}
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
