8fec4a0f2bfae504ddc909ffac9864afd76d3aed
Chiran Gurung <grgchiran08@yahoo.com>
2023-10-17 22:57:47 -0400

    some changes for product details

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,88 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./components/Main";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import ProductDetails from "./components/ProductDetails";
+
+import sneaker1 from "./assets/images/sneaker-1.png";
+import sneaker3 from "./assets/images/sneaker-3.png";
+import sneaker2 from "./assets/images/sneaker-2.png";
+import sneaker4 from "./assets/images/sneaker-4.png";
+import sneaker5 from "./assets/images/sneaker-5.png";
+import sneaker6 from "./assets/images/sneaker-6.png";
 
 export default function App() {
+  const products = [
+    {
+      id: 1,
+      name: "Nike Men's Court Vision Next Nature Shoes, Sneakers, Mid Top",
+      price: "$85.95",
+      sizes: ["8", "9", "10", "11", "12"],
+      imageSrc: sneaker1,
+      imageAlt: "Nike Men's Court Vision Next Nature Shoes, Sneakers, Mid Top",
+      details:
+        "In love with the classic look of '80s basketball but have a thing for the fast-paced culture of today's game? Meet the Nike Court Vision Mid. A classic remixed with at least 20% recycled materials by weight. Nike replaced leather with recycled and synthetic materials that keeps the soul of the original style. Its crisp upper and stitched overlays stays true to its origins. The plush collar keeps it sleek and comfortable for your world. ",
+    },
+    {
+      id: 2,
+      name: "Men's Grand Court 2.0 Cloudfoam Comfort Sneakers",
+      price: "$89.99",
+      sizes: ["8", "9", "10", "11", "12"],
+      imageSrc: sneaker2,
+      imageAlt: "Men's Grand Court 2.0 Cloudfoam Comfort Sneakers",
+      details:
+        "These men's tennis-inspired shoes have a durable synthetic leather upper and a Cloudfoam Comfort sockliner for step-in cushioning. Classic-look 3-Stripes provide the finishing touch. This product represents just one of the adidas solutions to help end plastic waste.",
+    },
+    {
+      id: 3,
+      name: "Converse Men's Chuck Taylor All Star Ox Uni Hightop Sneakers - Black",
+      price: "$79.95",
+      sizes: ["8", "9", "10", "11", "12"],
+      imageSrc: sneaker3,
+      imageAlt:
+        "Converse Men's Chuck Taylor All Star Ox Uni Hightop Sneakers - Black",
+      details:
+        "The Chuck Taylor All Star is the shoe that started it all for Converse. These hightop sneakers have the iconic canvas uppers and vulcanized rubber outsoles. There've been countless imitations - the star-centered patch, rubber toe cap, toe bumper and striping tell you these are the real thing.",
+    },
+    {
+      id: 4,
+      name: "PUMA Men's Softrider Enzo NXT Shoes",
+      price: "$95.99",
+      sizes: ["8", "9", "10", "11", "12"],
+      imageSrc: sneaker4,
+      imageAlt: "PUMA Men's Softrider Enzo NXT Shoes",
+      details:
+        "This is what happens when you combine two of PUMA's most innovative franchises: Softride and Enzo. A shoe that takes cushioning to the extreme, a shoe that provides unbelievable all-day comfort, a shoe that boasts a progressive upper design language with bold PUMA branding. Whether you want to stand out from the crowd, or leave your opponent far behind, these are the shoes for you.",
+    },
+    {
+      id: 5,
+      name: "PUMA Men's Classic XXI Shoes, Sneakers, Suede",
+      price: "$89.95",
+      sizes: ["8", "9", "10", "11", "12"],
+      imageSrc: sneaker5,
+      imageAlt: "PUMA Men's Classic XXI Shoes, Sneakers, Suede",
+      details:
+        "The Suede hit the scene in 1968 and has been changing the game ever since. While many sneakers have come and gone since then – leaving footprints of varying sizes – it's difficult to argue any shoe having as much of a deep-rooted impact on society and culture as the Suede. From Tommie Smith’s protest on the victory stand during the 1968 Olympic games to b-boy crews in NYC adopting the shoe in the 1980s. It’s been worn by the icons of every generation – and it’s stayed classic through it all. The PUMA Suede is an icon for everyone. The Classic XXI brings back PUMA's most iconic silhouette with a full suede upper and several quality improvements.",
+    },
+    {
+      id: 6,
+      name: "Nike Men's Air Jordan 1 Basketball Shoes",
+      price: "$155.95",
+      sizes: ["8", "9", "10", "11", "12"],
+      imageSrc: sneaker6,
+      imageAlt: "Nike Men's Air Jordan 1 Basketball Shoes",
+      details:
+        "Inspired by the original AJ1, this mid-top edition maintains the iconic look you love while choice colors and crisp leather give it a distinct identity.",
+    },
+  ];
   return (
     <Router>
       <Nav />
       <Routes>
-        <Route path="/" element={<Main />} />
-        {/* <Route path="/product-details" element={<ProductDetails />} /> */}
+        <Route path="/" element={<Main products={products} />} />
+        <Route
+          path="/product/:id"
+          element={<ProductDetails products={products} />}
+        />
       </Routes>
       <Footer />
     </Router>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,67 +1,4 @@
-import sneaker1 from "../assets/images/sneaker-1.png";
-import sneaker2 from "../assets/images/sneaker-2.png";
-import sneaker3 from "../assets/images/sneaker-3.png";
-import sneaker4 from "../assets/images/sneaker-4.png";
-import sneaker5 from "../assets/images/sneaker-5.png";
-import sneaker6 from "../assets/images/sneaker-6.png";
-function Main() {
-  const products = [
-    {
-      id: 1,
-      name: "Nike Men's Court Vision Next Nature Shoes, Sneakers, Mid Top",
-      href: "#",
-      price: "$85.95",
-      sizes: ["8", "9", "10", "11", "12"],
-      imageSrc: sneaker1,
-      imageAlt: "Nike Men's Court Vision Next Nature Shoes, Sneakers, Mid Top",
-    },
-    {
-      id: 2,
-      name: "Men's Grand Court 2.0 Cloudfoam Comfort Sneakers",
-      href: "#",
-      price: "$89.99",
-      sizes: ["8", "9", "10", "11", "12"],
-      imageSrc: sneaker2,
-      imageAlt: "Men's Grand Court 2.0 Cloudfoam Comfort Sneakers",
-    },
-    {
-      id: 3,
-      name: "Converse Men's Chuck Taylor All Star Ox Uni Hightop Sneakers - Black",
-      href: "#",
-      price: "$79.95",
-      sizes: ["8", "9", "10", "11", "12"],
-      imageSrc: sneaker3,
-      imageAlt:
-        "Converse Men's Chuck Taylor All Star Ox Uni Hightop Sneakers - Black",
-    },
-    {
-      id: 4,
-      name: "PUMA Men's Softrider Enzo NXT Shoes",
-      href: "#",
-      price: "$95.99",
-      sizes: ["8", "9", "10", "11", "12"],
-      imageSrc: sneaker4,
-      imageAlt: "PUMA Men's Softrider Enzo NXT Shoes",
-    },
-    {
-      id: 5,
-      name: "PUMA Men's Classic XXI Shoes, Sneakers, Suede",
-      href: "#",
-      price: "$89.95",
-      sizes: ["8", "9", "10", "11", "12"],
-      imageSrc: sneaker5,
-      imageAlt: "PUMA Men's Classic XXI Shoes, Sneakers, Suede",
-    },
-    {
-      id: 6,
-      name: "Nike Men's Air Jordan 1 Basketball Shoes",
-      href: "#",
-      price: "$155.95",
-      sizes: ["8", "9", "10", "11", "12"],
-      imageSrc: sneaker6,
-      imageAlt: "Nike Men's Air Jordan 1 Basketball Shoes",
-    },
-  ];
+function Main({ products }) {
   return (
     <>
       <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 text-center m-5 p-5 underline ">
@@ -72,7 +9,7 @@ function Main() {
           {products.map((product) => (
             <a
               key={product.id}
-              href={product.href}
+              href={`/product/${product.id}`}
               className="group border border-gray-200 flex flex-col overflow-hidden rounded-lg hover:shadow-xl"
             >
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
